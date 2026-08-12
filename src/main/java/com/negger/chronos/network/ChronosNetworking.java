61bc901adbe713d5e5756157ff94146bbab0bbb9
Couscous_taijine <50766221+CouscousTaijine@users.noreply.github.com>
2026-08-12 17:03:58 +0200

    Add rewind motion packet id

---
+++ src/main/java/com/negger/chronos/network/ChronosNetworking.java
@@ -0,0 +1,8 @@
+package com.negger.chronos.network;
+
+import net.minecraft.util.Identifier;
+
+public final class ChronosNetworking {
+    public static final Identifier REWIND_MOTION = new Identifier("chronos", "rewind_motion");
+    private ChronosNetworking() {}
+}
